58f20a90153eb2a138fcf6c468a6e30a330a7203
黎海冬 <lihaidong@lihaidongdeMac-mini.local>
2021-05-11 11:43:01 +0800

    release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-PUBLIC_URL=https://lhd-defactory.github.io/lhd-dfactory/
+echo PUBLIC_URL
 yarn build
-cp -R build/* ../lhd-defactory
+cp -R build/* ../lhd-dfactory
 cd ../lhd-defactory
 git add .
 git commit -m 'release'
