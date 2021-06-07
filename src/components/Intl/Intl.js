193146ba6bdc76eccf49a81dc98f5e6d8c4d9be7
黎海冬 <haidong.li@dfactory.tech>
2021-06-07 15:35:44 +0800

    dev intl

---
+++ src/components/Intl/Intl.js
@@ -0,0 +1,5 @@
+export default function Intl(){
+  return(
+    <div></div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import './assets/deri.less';
 import { Provider } from 'mobx-react';
 import Wallet from './model/Wallet';
-//note dont remove zhe line
-// import NumberPolyfill from './lib/polyfill/numberPolyfill'
 import Trading from './model/Trading';
 import { DeriEnv } from './lib/web3js/indexV2';
 import Version from './model/Version';
