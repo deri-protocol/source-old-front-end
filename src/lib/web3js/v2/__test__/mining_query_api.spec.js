fd9764a5947a759ec27d35b4694b1363e88c5b97
黎海冬 <haidong.li@dfactory.tech>
2021-06-09 10:21:41 +0800

    migrate imeme pool

--- src/lib/web3js/v2/__test__/mining_query_api.spec.js
+++ src/lib/web3js/v2/__test__/mining_query_api.spec.js
@@ -3,6 +3,7 @@ import {
   getLiquidityInfo,
 } from '../api/mining_query_api'
 import fetch from 'node-fetch'
+import { DeriEnv } from '../../config'
 global.fetch = fetch
 
 const TIMEOUT=20000
@@ -15,11 +16,14 @@ describe('Mining query api', () => {
   }, TIMEOUT)
   it('getLiquidityInfo()', async() => {
     const input = ['97', '0x19EC6281749C06Ed9647134c57257AcA1508bFA8', '0xFFe85D82409c5b9D734066C134b0c2CCDd68C4dF', '0', true]
+    //DeriEnv.set('prod')
+    //const input = ['56', '0x19c2655A0e1639B189FB0CF06e02DC0254419D92', '0xB09C45dE1934041d6B4d00917e087248D2aacc26', '0', true]
     const output = {
       poolLiquidity: '12187.754865076298332719',
       shares: '1169.993876306494702719',
       maxRemovableShares: '1169.993876306494702719',
     }
     expect(await getLiquidityInfo(...input)).toEqual(output)
+    //DeriEnv.set('dev')
   }, TIMEOUT)
 })
