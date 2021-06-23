531edf8b0dd09d0af4e96e765e9930b7b1929d59
黎海冬 <haidong.li@dfactory.tech>
2021-06-23 14:32:38 +0800

    optimize the release script

--- release.sh
+++ release.sh
@@ -2,23 +2,23 @@
 printf '\e[1;34m%-6s\e[m\n' "begin to release all site" 
 # app.deri.finance
 printf '\e[1;34m%-6s\e[m\n' "release app.deri.finance start" 
-./release_app.sh
+./release_app.sh silence >/dev/null 2>&1
 printf '\e[1;34m%-6s\e[m\n' "release app.deri.finance success" 
 # deri.finance
 printf '\e[1;34m%-6s\e[m\n' "release deri.finance start" 
-./release_index.sh
+./release_index.sh silence >/dev/null 2>&1
 printf '\e[1;34m%-6s\e[m\n' "release deri.finance success"
 # bridge.deri.finance
 printf '\e[1;34m%-6s\e[m\n' "release bridge.deri.finance start" 
-./release_bridge.sh
+./release_bridge.sh silence >/dev/null 2>&1
 printf '\e[1;34m%-6s\e[m\n' "release bridge.deri.finance success"
 # governance.deri.finance
 printf '\e[1;34m%-6s\e[m\n' "release governance.deri.finance start" 
-./release_governance.sh
+./release_governance.sh silence >/dev/null 2>&1
 printf '\e[1;34m%-6s\e[m\n' "release governance.deri.finance success"
 
 printf '\e[1;34m%-6s\e[m\n' "all site release success " 
 
 printf '\e[1;34m%-6s\e[m\n' "clear build dir"
-rf -rm ./build 
+rm -rf ./build 
 
--- release_app.sh
+++ release_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo PUBLIC_URL=https://cdn.jsdelivr.net/gh/deri-finance/app@main > .env.production
-yarn build --nomaps
+yarn build --nomaps 
 repo=$HOME/work/app
 
 if [ ! -d $repo ]; then
@@ -11,29 +11,32 @@ fi
 printf '\e[1;34m%-6s\e[m\n' 'git checkout main branch and fetch '
 git -C $repo checkout main
 git -C $repo pull origin main 
+
 printf '\e[1;34m%-6s\e[m\n' 'backup current version'
 cp $repo/index.html $repo/index.bak.html
 cp $repo/asset-manifest.json $repo/asset-manifest.bak.json
 cp $repo/manifest.json $repo/manifest.bak.json
-printf '\e[1;34m%-6s\e[m\n' 'copy to '$repo
 
+printf '\e[1;34m%-6s\e[m\n' 'copy to '$repo
 cp -R build/* $repo
 git -C $repo add .
 git -C $repo commit -m 'release app'
-read -r -p  "execute git push?[Y/n]" input
-case $input in
-    [yY][eE][sS]|[yY])
-printf '\e[1;34m%-6s\e[m\n' "git push origin main"
-git -C $repo push origin main
-exit
- ;;
-     [nN][oO]|[nN])
- echo "No"
- exit
-        ;;
- *)
-printf '\e[1;34m%-6s\e[m\n' "Invalid input..."
- exit
- ;;
- esac
+if [ $1 !='silence' ]; then
+  read -r -p  "execute git push?[Y/n]" input
+  case $input in
+      [yY][eE][sS]|[yY])
+  printf '\e[1;34m%-6s\e[m\n' "git push origin main"
+  git -C $repo push origin main
+  exit
+  ;;
+      [nN][oO]|[nN])
+  echo "No"
+  exit
+          ;;
+  *)
+  printf '\e[1;34m%-6s\e[m\n' "Invalid input..."
+  exit
+  ;;
+  esac
+fi
 
