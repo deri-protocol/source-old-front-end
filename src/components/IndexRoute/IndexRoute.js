380f36c99a4f6c76188354d9d91027a954f78222
黎海冬 <haidong.li@dfactory.tech>
2021-06-28 17:42:37 +0800

    optimize for alphatest

--- src/components/IndexRoute/IndexRoute.js
+++ src/components/IndexRoute/IndexRoute.js
@@ -2,11 +2,11 @@ import { Redirect } from "react-router-dom";
 
 export default function IndexRoute(){
   const url = window.location.href;
-  if(/https?:\/\/app/.test(url)) {
+  if(/^https?:\/\/(app|alphatest|testnet)/.test(url)) {
     return  <Redirect to='/lite'/> 
-  } else if(/https?:\/\/governance/.test(url)) {
+  } else if(/^https?:\/\/governance/.test(url)) {
     return  <Redirect to='/governance'/> 
-  } else if(/https?:\/\/bridge/.test(url)) {
+  } else if(/^https?:\/\/bridge/.test(url)) {
     return  <Redirect to='/bridge'/> 
   } 
   return <Redirect to='/index'/>
