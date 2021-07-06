5d082e089fe925e2d529f0292965882ac4546ae4
黎海冬 <haidong.li@dfactory.tech>
2021-07-06 16:28:08 +0800

    dev

---
+++ src/desktop/components/Mining/PoolList.js
@@ -0,0 +1,26 @@
+import PoolBox from '../../../components/Pool/PoolBox';
+import useMiningPool from '../../../hooks/useMiningPool';
+import './pool.less'
+import './zh-pool.less'
+import { inject, observer } from 'mobx-react';
+
+
+function Pool({version,lang}){
+  const [loaded,pools,v1Pools,v2Pools] = useMiningPool(version);
+  return (
+    <div className="mining-info">
+      <div className="pools">
+        {v2Pools.map((pool,index) => <PoolBox pool={pool} key={index} lang={lang}/>)}
+        {!loaded && <div className="loading">
+          <span
+            className="spinner spinner-border spinner-border-sm">
+            </span>
+          </div>}  
+      </div>
+      <div className='pools'>
+        {v1Pools.map((pool,index) => <PoolBox pool={pool} key={index} lang={lang}/>)}
+      </div>
+    </div>
+  )
+}
+export default inject('version')(observer(Pool))
