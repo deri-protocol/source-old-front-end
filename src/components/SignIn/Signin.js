3ff4402b668dd91f7d7520f8dbb228de2b683607
wenchao.huang <wenchao.huang@dfactory.tech>
2021-07-09 10:44:01 +0800

    fix

--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -3,11 +3,18 @@ import {inject,observer} from 'mobx-react'
 import success from './img/success.svg'
 import undone from './img/undone.svg'
 import right from './img/right.svg'
+import {
+  DeriEnv
+} from "../../lib/web3js/indexV2";
 import Button from '../Button/Button'
 function Signin({wallet={},lang}){
-  const spec = {
-    pool:"0x19c2655A0e1639B189FB0CF06e02DC0254419D92",
-    bTokenId:"0"
+  const isDev = DeriEnv.get() == 'dev' ? false : true;
+  const spec =  isDev?{
+    "pool":"0x19c2655A0e1639B189FB0CF06e02DC0254419D92",
+    "bTokenId":"0"
+  }:{
+    "pool":"0x54a71Cad29C314eA081b2B0b1Ac25a7cE3b7f7A5",
+    "bTokenId":"0"
   }
   const [isApprove, setIsApprove] = useState(true);
   const [isClaim,setIsClaim] = useState(false);
@@ -41,7 +48,7 @@ function Signin({wallet={},lang}){
       alert(lang['less-bnb'])
     }
     if(isNowSign){
-      alert('bbb')
+      alert(lang['use-a-new-address'])
     }
   }
 
