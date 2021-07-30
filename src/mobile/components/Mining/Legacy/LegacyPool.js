03bce77385f34f52c63d369e2ea3e17ba21d3c19
wenchao.huang <wenchao.huang@dfactory.tech>
2021-07-30 17:04:19 +0800

    fix

--- src/mobile/components/Mining/Legacy/LegacyPool.js
+++ src/mobile/components/Mining/Legacy/LegacyPool.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './legacyPools.less'
 import './zh-pool.less'
 import PoolBox from '../../../../components/Pool/legacy/LegacyPoolBox';
@@ -6,9 +6,12 @@ import useMiningPool from '../../../../hooks/useMiningPool';
 import { inject, observer } from 'mobx-react';
 
 
-function Pool({version,lang}){
+function Pool({version,lang,loading}){
   const [loaded,pools,v1Pools,v2Pools,legacy,premining] = useMiningPool(version);
-
+  useEffect(() => {
+    loaded ? loading.loaded() : loading.loading()
+    return () => {}
+  }, [loaded])
   return (
     <div className='retired'>
      <div className='retired-title'>
@@ -19,11 +22,7 @@ function Pool({version,lang}){
       <div className="retired-mining-info">
         <div className="pools">
           {legacy.map((pool,index) => <PoolBox pool={pool} key={index} lang={lang}/>)}
-          {!loaded && <div className="loading">
-            <span
-              className="spinner spinner-border spinner-border-sm">
-              </span>
-            </div>}  
+          
         </div>
       </div>
       <div className='retired-title'>
@@ -35,14 +34,10 @@ function Pool({version,lang}){
       <div className="retired-mining-info">
         <div className='pools'>
           {premining.map((pool,index) => <PoolBox pool={pool} key={index} lang={lang}/>)}
-          {!loaded && <div className="loading">
-            <span
-              className="spinner spinner-border spinner-border-sm">
-              </span>
-            </div>}  
+         
         </div>
       </div>
     </div>
   )
 }
-export default inject('version')(observer(Pool))
+export default inject('version','loading')(observer(Pool))
