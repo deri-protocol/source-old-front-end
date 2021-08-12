910f36b52b4502bbb07ef0b98e97963c6137d9f7
wenchao.huang <wenchao.huang@dfactory.tech>
2021-08-12 17:28:58 +0800

    airDrop

--- src/hooks/useMiningPool.js
+++ src/hooks/useMiningPool.js
@@ -119,13 +119,13 @@ export default function useMiningPool(isNew){
       const airDrop = {
         network : 'BSC',
         bTokenSymbol : 'GIVEAWAY',
-        liquidity : '34240',
+        liquidity : '1002200',
         symbol : '--',
         airdrop : true,
         chainId : 56,
         buttonText : 'CLAIM'
       }
-      // pools.push(airDrop)
+      pools.push(airDrop)
       let v1Pools = pools.filter(p => (p.version === 'v1' || !p.version) && !p.retired)
       let v2Pools = pools.filter(p => (p.version === 'v2' || p.version === 'v2_lite'  )&& !p.retired)
       const legacy = pools.filter(p => p.retired && !p.premining)
