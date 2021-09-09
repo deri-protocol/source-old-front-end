8e4c43fe382c9fb16b0d96468836afc602d06396
wenchao.huang <wenchao.huang@dfactory.tech>
2021-09-09 16:39:12 +0800

    fix

--- src/components/Nuls/Nuls.js
+++ src/components/Nuls/Nuls.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from 'react'
 import { inject, observer } from 'mobx-react'
@@ -11,7 +12,7 @@ function Nuls({ wallet = {}, lang }) {
   const [actionElement, setActionElement] = useState(<Button className='btn' btnText={lang['connect-wallet']}></Button>)
   const hasConnectWallet = () => wallet && wallet.detail && wallet.detail.account
   const [isApprove, setIsApprove] = useState(false);
-  const [isRegsiter, setIsRegsiter] = useState(false);
+  const [isClaim, setIsClaim] = useState(false);
   const connect = () => {
     wallet.connect()
   }
@@ -22,31 +23,37 @@ function Nuls({ wallet = {}, lang }) {
   const claimPtoken = async () => {
 
   }
-  const reg = async ()=>{
+  const reg = async () => {
 
   }
   useEffect(() => {
     let element;
     if (hasConnectWallet()) {
       if (!isApprove) {
-        element = <Button className='btn' btnText={lang['approve']} click={approve} lang={lang} />
+        element = <Button className='btn' btnText={lang['register']} click={approve} lang={lang} />
       } else {
-        if(isRegsiter){
+        if (isClaim) {
+          element = <a href='https://app.deri.finance/#/futures/pro' className='btn' />
+        } else {
           element = <Button className='btn' btnText={lang['claim']} click={claimPtoken} lang={lang} />
-        }else{
-          element = <Button className='btn' btnText={lang['register']} click={reg} lang={lang} />
         }
+
       }
     } else {
       element = <Button className='btn btn-danger connect' btnText={lang['connect-wallet']} click={connect} lang={lang} />
     }
     setActionElement(element)
-  }, [wallet.detail, isApprove,isRegsiter])
+  }, [wallet.detail, isApprove, isClaim])
 
   return (
     <div className='nuls-box'>
       <div className='title'>
-        {lang['title']}
+        <div>
+          {lang['title']}
+        </div>
+        <div className='time'>
+          {lang['in-celebration-of-the']}
+        </div>
       </div>
       <div className='logo'>
         <img className='nuls-img' src={nulsimg} />
@@ -61,7 +68,7 @@ function Nuls({ wallet = {}, lang }) {
             <div className='step'>
               <div className='step-img'>
                 <div>
-                  <img src={undone} />
+                  {isApprove ? <img src={success} /> : <img src={undone} />}
                 </div>
                 <div>
                   {lang['register']}
@@ -103,8 +110,57 @@ function Nuls({ wallet = {}, lang }) {
 
         </div>
       </div>
-      <div className='rule'>
-        <div className='rule-title'>
+      <div className='h2'>
+        {lang['title-one']}
+        <span className='title-num'>
+          {lang['title-num']}
+        </span>
+        <span className='in-deri'>
+          {lang['title-two']}
+        </span>
+        <div className='text'>
+          {lang['title-frou']}
+        </div>
+      </div>
+      <div className='rules'>
+        <div className='rules-title'>
+          {lang['rules']}
+        </div>
+        <div className='text'>
+          {lang['rules-one']}
+        </div>
+        <div className='text'>
+          {lang['rules-two']}
+        </div>
+        <div className='text'>
+          {lang['rules-three']}
+        </div>
+        <div className='rules-title'>
+          {lang['how-to-participate']}
+        </div>
+        <div className='text'>
+          {lang['step-one']}
+        </div>
+        <div className='text'>
+          {lang['step-two']}
+        </div>
+        <div className='text'>
+          {lang['step-three']}
+        </div>
+        <div className='rules-title'>
+          {lang['notes']}
+        </div>
+        <div className='text'>
+          {lang['notes-one']}
+        </div>
+        <div className='text'>
+          {lang['notes-two']}
+        </div>
+        <div className='text'>
+          {lang['notes-three']}
+        </div>
+        <div className='text'>
+          {lang['notes-four']}
         </div>
       </div>
     </div>
