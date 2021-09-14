f2438fbb72a75c6aa482bb31ee10408c2765f2bf
wenchao.huang <wenchao.huang@dfactory.tech>
2021-09-14 17:09:39 +0800

    fix

--- src/lib/web3js/activity/config.js
+++ src/lib/web3js/activity/config.js
@@ -1,17 +1,21 @@
 import { DeriEnv } from "../shared/config"
 
-export const getPTokenAirdropConfig = (chainId, env = 'dev') => {
+export const getPTokenAirdropConfig = (chainId, env) => {
   chainId = chainId.toString()
   env = env || DeriEnv.get();
   const config = {
-    prod: [{
-      chainId: '56',
-      address: '',
-    }],
-    dev: [{
-      chainId: '97',
-      address: '0x632D0f5d642B0d915cE1ad0772677FC589cc724d',
-    }],
+    prod: [
+      {
+        chainId: '56',
+        address: '0x055FD7f825cCD1b87092A1Ee40462c4c60dDD8ba',
+      },
+    ],
+    dev: [
+      {
+        chainId: '97',
+        address: '0x632D0f5d642B0d915cE1ad0772677FC589cc724d',
+      },
+    ],
   };
 
   if (Object.keys(config).includes(env)) {
