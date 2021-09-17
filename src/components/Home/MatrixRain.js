7c538cafbd5a62ad69b79d228d7d910a0dc63074
黎海冬 <haidong.li@dfactory.tech>
2021-09-17 18:15:38 +0800

    dev home

--- src/components/Home/MatrixRain.js
+++ src/components/Home/MatrixRain.js
@@ -28,11 +28,12 @@ export default function MatrixRain({lang}){
     canvas.height = rect.height;
 
     // Setting up the letters
-    var letters = 'Α Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101 Ν Ξ Ο Π Ρ Σ Τ Υ Φ Χ Ψ Ω α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ ς σ τ υ φ χ ψ ωΑ Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ Ν Ξ Ο Π Ρ Σ Τ Υ Φ Χ Ψ Ω α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ ς σ τ υ φ χ ψ ω';
+    var letters = 'ΑΒΓΔΕΖΗΘΙΚΛΜ0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101ΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψω';
     letters = letters.split('');
 
     // Setting up the columns
-    var fontSize = 14,
+    var fontsSizes = [24,18,14]
+    var fontSize = fontsSizes[0],
         columns = canvas.width / fontSize;
 
     // Setting up the drops
@@ -40,26 +41,42 @@ export default function MatrixRain({lang}){
     for (var i = 0; i < columns; i++) {
       drops[i] = 1;
     }
-
+    var random = 0
+    var selectedColumns= []
+    var selectedFontSize = fontsSizes[Math.floor(Math.random() * fontsSizes.length)]
+    for (let i = 0 ; i < Math.floor(columns / 0.8) ; i++){
+      selectedColumns.push(Math.floor(Math.random() * columns))
+    }
     // Setting up the draw function
     function draw() {
-      ctx.fillStyle = 'rgba(13, 14, 16, .1)';
+      ctx.fillStyle = 'rgba(13, 14, 16, 0.1)';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       for (var i = 0; i < drops.length; i++) {
         var text = letters[Math.floor(Math.random() * letters.length)];
-        ctx.fillStyle = '#0f0';
-
-        ctx.font = fontSize + "px arial";
-        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        if(selectedColumns.includes(i)){
+          ctx.font = selectedFontSize + 'px arial'
+          ctx.fillStyle = '#0068A3';
+        } else {
+          ctx.fillStyle = 'rgb(36,108,173)';
+          ctx.font = fontSize + "px arial";
+          random = 1
+        }
+        ctx.fillText(text, i * fontSize + random, drops[i] * fontSize);
         drops[i]++;
-        if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+        if (drops[i] * fontSize > canvas.height && Math.random() > .97) {
           drops[i] = 0;
+          if(selectedColumns.includes(i)){
+            var pos = selectedColumns.indexOf(i);
+            random = random + Math.floor(Math.random() * selectedFontSize)
+            selectedColumns.splice(pos,1,Math.floor(Math.random() * columns))
+            // selectedFontSize = fontsSizes[Math.floor(Math.random() * fontsSizes.length)]
+          }
         }
       }
     }
 
     // Loop the animation
-    setInterval(draw, 100);
+    setInterval(draw, 33);
   }, [])
   return (
     <canvas ref={canvasRef}>
