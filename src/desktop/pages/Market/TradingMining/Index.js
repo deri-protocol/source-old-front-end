54d038bfcf8fee8d99a10af143aaccd7cd6bfb4e
黎海冬 <haidong.li@dfactory.tech>
2021-09-30 09:42:44 +0800

    move countdown

--- src/desktop/pages/Market/TradingMining/Index.js
+++ src/desktop/pages/Market/TradingMining/Index.js
@@ -1,4 +1,4 @@
-import CountDown from "./CountDown";
+import CountDown from "../../../../components/Countdown/CountDown";
 
 export default function Index(){
   return (
@@ -11,7 +11,7 @@ export default function Index(){
 
       </div>
       <div className='trading'>
-        
+
       </div>
     </div>
   )
