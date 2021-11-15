5ced6b0458d8dcdeaaf3cdd2d549f1484b6b7de5
wenchao.huang <wenchao.huang@dfactory.tech>
2021-11-15 18:26:50 +0800

    fix

--- src/components/TradingMining/one/Claim.js
+++ src/components/TradingMining/one/Claim.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { inject, observer } from 'mobx-react';
 import DeriNumberFormat from '../../../utils/DeriNumberFormat'
 import TipWrapper from '../../TipWrapper/TipWrapper'
-import { getUserStakingReward, getUserStakingClaimInfo, claimMyStaking,bg } from '../../../lib/web3js/indexV2'
+import { getUserStakingReward, getUserStakingClaimInfo, claimMyStaking, bg } from '../../../lib/web3js/indexV2'
 import Button from '../../Button/Button'
 function Claim({ wallet, lang }) {
   const [claimReward, setClaimReward] = useState('')
@@ -12,41 +12,43 @@ function Claim({ wallet, lang }) {
   const [lockedDeri, setLockedDeri] = useState('')
   const price = 0.5054307301
   const getReward = async () => {
-    let res = await getUserStakingReward(wallet.detail.account,1)
-    let deri = res.rewardDERI / price
-    setClaimReward(res.rewardDERI)
-    setClaimRewardDeri(deri)
-  }     
+    let res = await getUserStakingReward(wallet.detail.account, 1)
+    if (res) {
+      let deri = (res.rewardDERI / price).toString()
+      setClaimReward(res.rewardDERI)
+      setClaimRewardDeri(deri)
+    }
+  }
   const claim = async () => {
-    if(+claimableDeri === 0){
+    if (+claimableDeri === 0) {
       return;
     }
 
-    let nowSixEnd = new Date(new Date().toLocaleDateString()).getTime() + 18.5 * 60 * 60 * 1000 
+    let nowSixEnd = new Date(new Date().toLocaleDateString()).getTime() + 18.5 * 60 * 60 * 1000
     let nowSix = new Date(new Date().toLocaleDateString()).getTime() + 18 * 60 * 60 * 1000
     let now = parseInt(Date.now())
-    if(now < nowSixEnd && now > nowSix){
+    if (now < nowSixEnd && now > nowSix) {
       alert('Claiming DERI is disabled during first 30 minutes of each epoch')
       return;
     }
 
-    let res = await claimMyStaking(wallet.detail.account,1)
-    if(res.success){
+    let res = await claimMyStaking(wallet.detail.account, 1)
+    if (res.success) {
       let claimed = bg(claimedDeri).plus(bg(claimableDeri)).toString()
       setClaimedDeri(claimed)
       setClaimableDeri(0)
     }
   }
-  useEffect(()=>{
-    if(+claimableDeri === 0){
+  useEffect(() => {
+    if (+claimableDeri === 0) {
       document.getElementsByClassName('claim-button')[0].style.color = '#AAA'
-    }else{
+    } else {
       document.getElementsByClassName('claim-button')[0].style.color = '#FFF'
-    }   
-  },[claimableDeri])
+    }
+  }, [claimableDeri])
   const getClaimInfo = async () => {
-    let res = await getUserStakingClaimInfo(wallet.detail.account,1)
-    if(res){
+    let res = await getUserStakingClaimInfo(wallet.detail.account, 1)
+    if (res) {
       setClaimedDeri(res.claimed)
       setClaimableDeri(res.claimable)
       setLockedDeri(res.locked)
@@ -95,7 +97,7 @@ function Claim({ wallet, lang }) {
                   </TipWrapper>
                 </div>
                 <div className='unclaimed-num'>
-                  <DeriNumberFormat value={lockedDeri} decimalScale={2} thousandSeparator={true} /> 
+                  <DeriNumberFormat value={lockedDeri} decimalScale={2} thousandSeparator={true} />
                 </div>
               </div>
               <div className='cur-epoch-claimable-deri'>
@@ -112,7 +114,9 @@ function Claim({ wallet, lang }) {
                 <Button className='claim-button' btnText='CLAIM' click={claim} lang={lang}></Button>
               </div>
             </div>
-
+          </div>
+          <div className='text-waring'>
+          the referenced Deri Price is $0.5054307301 as snapshoted on coinmarketcap.com at 10:00 UTC Nov.10th
           </div>
         </div>
       </div>
