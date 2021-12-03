4c75bf2bd1befe73ecc372caee62543e4c9bcaee
wenchao.huang <wenchao.huang@dfactory.tech>
2021-12-03 09:56:58 +0800

    migrate

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const trading = new Trading()
 if(process.env.NODE_ENV === 'production') {
   DeriEnv.set('prod')
 }
-DeriEnv.set('dev')
+DeriEnv.set('prod')
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
